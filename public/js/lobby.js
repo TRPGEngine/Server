d89e994d2338df19d20087beb37c851e4585f7fe
moonrailgun <moonrailgun@gmail.com>
2017-07-02 15:54:58 +0800

    房间信息刷新

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -16,11 +16,20 @@ $(function() {
     console.log('连接断开');
   });
 
+  // 房间列表点击获取房间信息
   $('.room-list .room-item').click(function() {
     var roomId = $(this).data('roomId');
     socket.emit('core::getRoomInfo', {roomId: roomId}, function(data) {
       console.log('core::getRoomInfo', data);
       $('.roomInfo').html(roomInfoTpl(data)).animate({width: '400px'});
     })
-  })
+  });
+  // 房间信息刷新按钮
+  $('.roomInfo').on('click', 'i.fa-refresh', function() {
+    var roomId = $(this).closest('.roomInfo>div').data('roomId')
+    socket.emit('core::getRoomInfo', {roomId: roomId}, function(data) {
+      console.log('core::getRoomInfo', data);
+      $('.roomInfo').html(roomInfoTpl(data));
+    })
+  });
 });
