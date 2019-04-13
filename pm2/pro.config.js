3d11667aac0047d9ee0b269a013981695bbc2a23
moonrailgun <moonrailgun@gmail.com>
2019-04-13 12:59:43 +0800

    :hammer: add pm2

---
+++ pm2/pro.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  apps: [
+    {
+      name: 'trpg-server',
+      script: 'standard.js',
+      cwd: path.resolve(__dirname, '../'),
+      max_restarts: 10,
+      env: {
+        NODE_ENV: 'production',
+        PORT: '23256',
+        VERBOSE: 'true',
+        DEBUG: 'trpg:*',
+      },
+    },
+  ],
+};
