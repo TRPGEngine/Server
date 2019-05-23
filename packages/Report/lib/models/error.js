f63f7ad7b65f794a517ebb0177b25a76cbfe5d39
moonrailgun <moonrailgun@gmail.com>
2019-05-23 10:12:38 +0800

    :hammer: [report] 调整表字段大小防止数据太长无法记录

--- packages/Report/lib/models/error.js
+++ packages/Report/lib/models/error.js
@@ -4,7 +4,7 @@ module.exports = function ReportError(Sequelize, db) {
     ua: { type: Sequelize.STRING },
     version: { type: Sequelize.STRING },
     message: { type: Sequelize.STRING, required: true },
-    stack: { type: Sequelize.STRING(1000), required: true },
+    stack: { type: Sequelize.TEXT, required: true },
   });
 
   return ReportError;
