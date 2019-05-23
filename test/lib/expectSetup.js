f5acf698ed9d9edf56287e9438a78e58a35d9f19
moonrailgun <moonrailgun@gmail.com>
2019-05-23 16:04:58 +0800

    :hammer: 修改与调整一些细节
    文件权限
    代码美化
    包命令
    ts配置

--- test/lib/expectSetup.js
+++ test/lib/expectSetup.js
@@ -2,16 +2,17 @@ const jestExpect = global.expect;
 
 jestExpect.extend({
   toBeSuccess: (received) => {
-    if(received.result === true) {
+    if (received.result === true) {
       return {
-        message: () => `result should be error but successd: ${JSON.stringify(received)}`,
+        message: () =>
+          `result should be error but successd: ${JSON.stringify(received)}`,
         pass: true,
       };
-    }else {
+    } else {
       return {
         message: () => `result is ${received.result}, message: ${received.msg}`,
         pass: false,
       };
     }
-  }
+  },
 });
