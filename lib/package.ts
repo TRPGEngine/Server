bdc0f6a0305ee3a70a5c34a4c1d4d71777979659
moonrailgun <moonrailgun@gmail.com>
2019-06-05 17:00:40 +0800

    :hammer: 调整一些包的细节与配置

--- lib/package.ts
+++ lib/package.ts
@@ -3,9 +3,9 @@ import Router from 'koa-router';
 import Debug, { Debugger } from 'debug';
 
 export default abstract class BasePackage {
-  public abstract name: string;
-  public abstract require: string[];
-  public desc: string;
+  public abstract name: string; // 包名
+  public abstract require: string[]; // 包依赖列表
+  public abstract desc: string; // 包信息描述
   private _debug: Debugger;
   private _app: TRPGApplication;
   private _models: Model[]; // 该包注册的数据库模型列表
@@ -16,6 +16,17 @@ export default abstract class BasePackage {
 
     this._debug = Debug(`trpg:package:${this.getPackageName()}`);
   }
+
+  /**
+   * 被加载时的回调
+   */
+  onLoad(): void {}
+
+  /**
+   * 被初始化的回调
+   * 需要被实现
+   */
+  abstract onInit(): Promise<void>;
 
   get app() {
     return this._app;
@@ -36,13 +47,13 @@ export default abstract class BasePackage {
     return this.name;
   }
 
-  regModel(modelFn: ModelFn) {
+  protected regModel(modelFn: ModelFn) {
     const storage = this.storage;
     const model = storage.registerModel(modelFn);
     this._models.push(model);
   }
 
-  regSocketEvent(name: string, event: SocketEventFn) {
+  protected regSocketEvent(name: string, event: SocketEventFn) {
     const app = this.app;
     const packageName = this.name;
     if (!name.startsWith(`${packageName}::`)) {
@@ -53,11 +64,14 @@ export default abstract class BasePackage {
     app.registerEvent(name, event);
   }
 
-  regRoute(path: string, route: Router) {
+  protected regRoute(path: string, route: Router) {
     this.router.use(path, route.routes());
   }
 
-  initCompleted() {
+  /**
+   * 初始化完毕后的回调
+   */
+  onInitCompleted() {
     const app = this.app;
 
     if (this._router) {
@@ -74,9 +88,15 @@ export default abstract class BasePackage {
     );
   }
 
+  // TODO
   regValue(value: {}) {}
   regMethod(method: any) {}
 
+  /**
+   * 输出调试信息
+   * @param formatter 调试信息的格式
+   * @param args 调试信息的参数
+   */
   debug(formatter: any, ...args: any[]) {
     this._debug(formatter, ...args);
   }
