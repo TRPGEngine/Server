efa25f618ee54babd520679265236826c02ec15f
moonrailgun <moonrailgun@gmail.com>
2019-07-10 20:09:46 +0800

    :hammer: [File] bump to ts and rewrite file path param

---
+++ packages/File/lib/middleware/upload.ts
@@ -0,0 +1,31 @@
+import multer from 'koa-multer'; //加载koa-multer模块
+import config from '../config';
+import _ from 'lodash';
+
+/**
+ * 上传文件中间件
+ * @param path 文件上传路径，可以为boolean型， 如果为true则上传到永久目录，如果为false则上传到临时目录， 默认为false
+ */
+export default function uploadMiddleware(path: string | boolean = false) {
+  return multer({
+    storage: multer.diskStorage({
+      //文件保存路径
+      destination: function(req, file, cb) {
+        if (typeof path === 'boolean') {
+          path = path
+            ? 'public/uploads/persistence/'
+            : 'public/uploads/temporary/';
+        }
+
+        cb(null, path);
+      },
+      //修改文件名称
+      filename: function(req, file, cb) {
+        const ext = _.last(file.originalname.split('.'));
+        const filename = `${Date.now()}.${ext}`;
+        cb(null, filename);
+      },
+    }),
+    limits: config.limits,
+  });
+}
