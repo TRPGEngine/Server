d3a2d5d1e15d4183315f7e95851a0537697bfa8e
moonrailgun <moonrailgun@gmail.com>
2019-07-19 16:13:36 +0800

    :hammer: [db] 修改增加内置人物卡模板的方法
    从原来的批量删除后批量新增改为批量软删除后单点查询恢复更新字段
    以name的值作为唯一标识

---
+++ db/seeders/3-seeder-dump_builtin_actor_template.js
@@ -0,0 +1,63 @@
+'use strict';
+/**
+ * 3-seeder-dump_builtin_actor_template.js
+ *
+ */
+
+const path = require('path');
+const fs = require('fs-extra');
+const layoutDir = path.resolve(__dirname, './layout/');
+const dirs = fs.readdirSync(layoutDir);
+
+const records = [];
+for (const name of dirs) {
+  const info = fs.readJsonSync(path.resolve(layoutDir, name, 'info.json'));
+  const layout = fs.readFileSync(path.resolve(layoutDir, name, 'layout.xml'), {
+    encoding: 'utf-8',
+  });
+
+  records.push({
+    name: info.name,
+    desc: info.desc,
+    info: JSON.stringify(info),
+    layout,
+    built_in: true,
+  });
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize, { app }) => {
+    // 先移除所有的built-in的字段
+    const db = app.storage.db;
+    await db.models.actor_template.destroy({ where: { built_in: true } });
+
+    for (const record of records) {
+      const name = record.name;
+      console.log('handle dump built-in template', name);
+      const template = await db.models.actor_template.findOne({
+        where: {
+          name,
+          built_in: true,
+        },
+        paranoid: false,
+      });
+      if (template) {
+        // 已存在，更新其他内容
+        console.log('update template');
+        await template.restore();
+        template.desc = record.desc;
+        template.info = record.info;
+        template.layout = record.layout;
+        template.deletedAt = null;
+        await template.save();
+      } else {
+        // 不存在, 创建
+        console.log('create template');
+        await db.models.actor_template.create(record);
+      }
+    }
+  },
+  down: (queryInterface, Sequelize) => {
+    // Optional, here code your db remove row
+  },
+};
