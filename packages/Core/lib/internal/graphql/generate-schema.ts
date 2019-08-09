c4f976257b449e9ae003377119c0c5cb9095297f
moonrailgun <moonrailgun@gmail.com>
2019-08-09 22:46:25 +0800

    :star: [Core] graphql 增加外键字段关系实现

--- packages/Core/lib/internal/graphql/generate-schema.ts
+++ packages/Core/lib/internal/graphql/generate-schema.ts
@@ -27,7 +27,7 @@ typeMapper.mapType((type: DataType) => {
             .trim()
             .replace(/([^_a-zA-Z0-9])/g, (_, p) => toPinyin(p) || ' ') // 转化类型时中文需要转化为拼音
             .split(' ')
-            .map((v, i) => (i ? _.upperFirst(v) : v))
+            .map((v: string, i: number) => (i ? _.upperFirst(v) : v))
             .join('')
             .replace(/(^\d)/, '_$1');
         })
@@ -38,6 +38,46 @@ typeMapper.mapType((type: DataType) => {
 
   return false;
 });
+
+/**
+ * 在fields增加外键关联字段
+ * 规则为 fk_${外键字段名(不包括Id)}
+ * @param queryConfig 目标查询配置文件
+ * @param modelCls 要处理的Sequelize模型
+ * @param fields 生成的fields
+ */
+function appendFieldsFkAssociations(
+  queryConfig: GraphQLObjectTypeConfig<any, any>,
+  modelCls: typeof Model,
+  fields: any
+) {
+  Object.keys(modelCls.associations).map((name) => {
+    const association = _.get(modelCls, ['associations', name]);
+
+    const associationType = association.associationType;
+    if (associationType === 'BelongsTo') {
+      // 一对多关系
+      // 即当前模型上有外键字段指向外部模型的id
+      const targetModel = association.target; // 目标模型
+      const identifierField = association.identifierField; // 连接字段
+      const type = _.get(queryConfig, ['fields', targetModel.name, 'type']);
+      if (!_.isNil(type)) {
+        _.set(fields, `fk_${name}`, {
+          type, // 返回 GraphQLObjectType
+          args: Object.assign({}, defaultArgs(targetModel), defaultListArgs()),
+          resolve: resolver(targetModel, {
+            list: false,
+            before: (findOptions, args, context, info) => {
+              const id = _.get(info, ['source', identifierField]);
+              findOptions.where = { id };
+              return findOptions;
+            },
+          }),
+        });
+      }
+    }
+  });
+}
 
 /**
  * 添加配置项到query config
@@ -93,6 +133,8 @@ export function generateSchema(db: Sequelize): GraphQLSchema {
   };
   _.forEach(models, (modelCls: typeof Model, modelName: string) => {
     const fields = attributeFields(modelCls);
+    // fields加入关联字段
+    appendFieldsFkAssociations(queryConfig, modelCls, fields);
 
     // 加入单项查询
     appendQueryConfig(queryConfig, modelCls, fields, false);
