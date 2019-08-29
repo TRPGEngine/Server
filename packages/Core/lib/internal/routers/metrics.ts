5e4451ed7970867797677544aa94812991ebcfba
moonrailgun <moonrailgun@gmail.com>
2019-08-29 09:43:16 +0800

    :star: [Core] 增加请求统计接口缓存层

--- packages/Core/lib/internal/routers/metrics.ts
+++ packages/Core/lib/internal/routers/metrics.ts
@@ -3,6 +3,12 @@ const router = new Router();
 import moment from 'moment';
 import { CoreMetrics } from '../models/metrics';
 import _ from 'lodash';
+import memoizeOne from 'memoize-one';
+
+// 获取统计信息的方法
+const getStatisInfo = memoizeOne((startDate, endDate) => {
+  return CoreMetrics.getStatisInfo(startDate, endDate);
+});
 
 router.get('/metrics/all', async (ctx) => {
   const startDate = ctx.params.start
@@ -10,7 +16,10 @@ router.get('/metrics/all', async (ctx) => {
     : moment().subtract(7, 'days');
   const endDate = ctx.params.end ? moment(ctx.params.end) : moment();
 
-  const statis = await CoreMetrics.getStatisInfo(startDate, endDate);
+  // 如果开始时间或结束时间不填，则会一直变。
+  // 则不会应用缓存
+  // 如果为固定值，则会应用缓存
+  const statis = await getStatisInfo(startDate, endDate);
 
   ctx.body = statis;
 });
