09e549f3fe51c34126c4062350d901384376874c
moonrailgun <moonrailgun@gmail.com>
2019-11-22 11:24:46 +0800

    :star: [Group] 增加/group/:groupUUID/actor/apply接口
    用于处理团人物卡申请

--- packages/Group/lib/routers/actor.ts
+++ packages/Group/lib/routers/actor.ts
@@ -1,7 +1,13 @@
 import { TRPGRouter } from 'trpg/core';
 import { GroupGroup } from '../models/group';
+import { GroupActor } from '../models/actor';
+import { ssoAuth } from 'packages/Player/lib/middleware/auth';
+import { PlayerJWTPayload } from 'packages/Player/types/player';
+import _ from 'lodash';
 
-const actorRouter = new TRPGRouter();
+const actorRouter = new TRPGRouter<{
+  player?: PlayerJWTPayload;
+}>();
 
 /**
  * 团目前人物卡列表
@@ -12,6 +18,29 @@ actorRouter.get('/:groupUUID/actor/list', async (ctx) => {
   const actors = await GroupGroup.findGroupActorsByUUID(groupUUID);
 
   ctx.body = { list: actors };
+});
+
+actorRouter.post('/:groupUUID/actor/apply', ssoAuth(), async (ctx) => {
+  const groupUUID = ctx.params.groupUUID;
+  const { actorUUID } = ctx.request.body;
+  const player = ctx.state.player;
+  const playerUUID = player.uuid;
+
+  if (_.isNil(groupUUID) || _.isNil(actorUUID) || _.isNil(playerUUID)) {
+    throw new Error('缺少必要字段');
+  }
+
+  if (!_.isString(actorUUID)) {
+    throw new Error('actorUUID 必须为字符串');
+  }
+
+  const actor = await GroupActor.addApprovalGroupActor(
+    groupUUID,
+    actorUUID,
+    playerUUID
+  );
+
+  ctx.body = { actor };
 });
 
 export default actorRouter;
