b56cbc57a3042d1a04b89e0ea760bcbfc9c27b28
moonrailgun <moonrailgun@gmail.com>
2019-11-24 20:43:53 +0800

    :hammer: [ci] add ci config and update config

---
+++ config/ci.js
@@ -0,0 +1,14 @@
+module.exports = {
+  port: 23266,
+  db: {
+    database: 'trpg_ci',
+    options: {
+      benchmark: true,
+    },
+    pool: {
+      max: 1,
+      min: 0,
+    },
+  },
+  redisUrl: 'redis://127.0.0.1:6379',
+};
