e03c652148cfe9a5ac06710471b4c86c17d4347f
moonrailgun <moonrailgun@gmail.com>
2020-02-16 19:48:05 +0800

    feat(chat): 增加多人会话的创建

--- packages/Chat/lib/models/converse.ts
+++ packages/Chat/lib/models/converse.ts
@@ -3,11 +3,22 @@ import {
   DBInstance,
   Orm,
   BelongsToSetAssociationMixin,
+  BelongsToManyAddAssociationsMixin,
+  BelongsToManyGetAssociationsMixin,
 } from 'trpg/core';
 import { PlayerUser } from 'packages/Player/lib/models/user';
+import _ from 'lodash';
 
-export type ChatConverseType = 'user' | 'channel' | 'group' | 'system';
+export type ChatConverseType =
+  | 'user'
+  | 'channel'
+  | 'group'
+  | 'system'
+  | 'multiuser'; // 多用户
 
+/**
+ * 多人会话的模型
+ */
 export class ChatConverse extends Model {
   uuid: string;
   type: ChatConverseType;
@@ -15,6 +26,61 @@ export class ChatConverse extends Model {
   icon: string;
 
   setOwner: BelongsToSetAssociationMixin<PlayerUser, number>;
+  addParticipants: BelongsToManyAddAssociationsMixin<PlayerUser, number>;
+  getParticipants: BelongsToManyGetAssociationsMixin<PlayerUser>;
+
+  /**
+   * 创建多人会话
+   */
+  static async createMultiConverse(name: string, userUUIDs: string[]) {
+    const num = userUUIDs.length;
+
+    if (num < 3) {
+      throw new Error('创建多人会话的人数不能少于三人');
+    }
+    if (num > 20) {
+      throw new Error('创建多人会话的人数过多');
+    }
+
+    const users: PlayerUser[] = await Promise.all(
+      userUUIDs.map((uuid) => PlayerUser.findByUUID(uuid))
+    );
+
+    if (_.isEmpty(name)) {
+      // 生成默认会话名
+      let defaultName = _.take(users, 3)
+        .map((u) => u.getName())
+        .join(',');
+      if (userUUIDs.length > 3) {
+        defaultName += '等人';
+      }
+
+      defaultName += '的多人会话';
+
+      name = defaultName;
+    }
+
+    const trpgapp = ChatConverse.getApplication();
+    return await trpgapp.storage.transaction(
+      'createMultiConverse',
+      async (transaction) => {
+        const converse: ChatConverse = await ChatConverse.create(
+          {
+            type: 'multiuser',
+            name,
+          },
+          {
+            transaction,
+          }
+        );
+        await converse.addParticipants(users, {
+          transaction,
+        });
+
+        return converse;
+      }
+    );
+  }
 }
 
 export default function ChatConverseDefinition(Sequelize: Orm, db: DBInstance) {
@@ -26,7 +92,7 @@ export default function ChatConverseDefinition(Sequelize: Orm, db: DBInstance) {
         required: true,
       },
       type: {
-        type: Sequelize.ENUM('user', 'channel', 'group', 'system'),
+        type: Sequelize.ENUM('user', 'channel', 'group', 'system', 'multiuser'),
         defaultValue: 'user',
       },
       name: { type: Sequelize.STRING },
---
+++ packages/Chat/test/model.spec.ts
@@ -0,0 +1,30 @@
+import { ChatConverse } from '../lib/models/converse';
+import { getTestUser, getOtherTestUser } from 'packages/Player/test/example';
+import { buildAppContext } from 'test/utils/app';
+
+const context = buildAppContext();
+
+describe('chatconverse model', () => {
+  test('ChatConverse.createMultiConverse should be ok', async () => {
+    const testUser1 = await getTestUser();
+    const testUser2 = await getOtherTestUser('admin9');
+    const testUser3 = await getOtherTestUser('admin8');
+    const uuids = [testUser1.uuid, testUser2.uuid, testUser3.uuid];
+
+    const converse = await ChatConverse.createMultiConverse(undefined, uuids);
+
+    try {
+      const participants = await converse.getParticipants();
+      expect(participants).toHaveLength(3);
+      expect(new Set(participants.map((x) => x.uuid))).toMatchObject(
+        new Set(uuids)
+      ); // 无序比较
+
+      expect(typeof converse.name).toBe('string');
+      expect(converse.name.startsWith(testUser1.getName())).toBe(true);
+      expect(converse.name.endsWith('的多人会话')).toBe(true);
+    } finally {
+      await converse.destroy();
+    }
+  });
+});
