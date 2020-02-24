0c80e0b958b6e9c37bc801079d9e88d623dc82ee
moonrailgun <moonrailgun@gmail.com>
2020-02-24 11:51:11 +0800

    perf(file): 优化置空旧绑定uuid操作2合1
    并丰富单元测试用例

--- packages/File/lib/middleware/storage/avatar.ts
+++ packages/File/lib/middleware/storage/avatar.ts
@@ -16,21 +16,21 @@ export default function avatarStorage(): TRPGMiddleware {
     const filename = _.get(ctx.req, 'file.filename');
     const size = _.get(ctx.req, 'file.size');
     const has_thumbnail = _.get(ctx.req, 'file.has_thumbnail', false);
-    const type = ctx.header['avatar-type'] || 'actor';
     const { width, height } = ctx.header;
-    const db = trpgapp.storage.db;
+    const type = ctx.header['avatar-type'] || 'actor';
     const attach_uuid: string = ctx.header['attach-uuid'] || null;
-    await db.transactionAsync(async () => {
+    await trpgapp.storage.transaction('uploadAvatar', async () => {
       if (attach_uuid) {
         // attach_uuid应唯一:一个用户只能有一个对应的头像文件、一个角色只能有一个对应的图片
         // 没有attach_uuid的文件会被定时删除
-        let oldAvatars = await FileAvatar.findAll({
-          where: { attach_uuid, type },
-        });
-        for (let oldAvatar of oldAvatars) {
-          oldAvatar.attach_uuid = null;
-          await oldAvatar.save();
-        }
+        await FileAvatar.update(
+          {
+            attach_uuid: null,
+          },
+          {
+            where: { attach_uuid, type },
+          }
+        );
       }
       const avatar: FileAvatar = await FileAvatar.create({
         name: filename,
--- packages/File/test/integration/avatar.spec.ts
+++ packages/File/test/integration/avatar.spec.ts
@@ -3,6 +3,8 @@ import path from 'path';
 import { getTestUser } from 'packages/Player/test/example';
 import { createTestActor } from 'packages/Actor/test/example';
 import testExampleStack from 'test/utils/example';
+import { FileAvatar } from 'packages/File/lib/models/avatar';
+import { Op } from 'trpg/core';
 
 const context = buildAppContext();
 
@@ -12,24 +14,59 @@ describe('chatimg router', () => {
   it('/file/avatar', async () => {
     const testUser = await getTestUser();
     const testActor = await createTestActor();
+    const file = path.resolve(__dirname, '../example/example-image.png');
+    const header = {
+      'user-uuid': testUser.uuid,
+      'avatar-type': 'actor',
+      'attach-uuid': testActor.uuid,
+    };
     const { body } = await context.request.upload(
       '/file/avatar',
       [
         {
           name: 'avatar',
-          file: path.resolve(__dirname, '../example/example-image.png'),
+          file,
         },
       ],
-      {
-        'user-uuid': testUser.uuid,
-        'avatar-type': 'actor',
-        'attach-uuid': testActor.uuid,
-      }
+      header
     );
 
     expect(body).toBeSuccess();
     expect(body).toHaveProperty('uuid');
     expect(body).toHaveProperty('url');
     expect(body).toHaveProperty('avatar');
+    expect(body).toHaveProperty('filename');
+    const firstAvatarUUID = body.uuid;
+
+    // 再次上传会将上一次的绑定的uuid置空
+    const { body: body2 } = await context.request.upload(
+      '/file/avatar',
+      [
+        {
+          name: 'avatar',
+          file,
+        },
+      ],
+      header
+    );
+    expect(body2.uuid).not.toEqual(firstAvatarUUID); // 两次上传同样文件, 应该生成两条记录
+    expect(body2.filename).toEqual(body.filename); // 两次上传同样文件, 文件名应相等
+
+    const firstAvatar = await FileAvatar.findOne({
+      where: {
+        uuid: firstAvatarUUID,
+      },
+    });
+    expect(firstAvatar.attach_uuid).toBeNull(); // 旧的记录的attach_uuid应当被置空
+
+    // 测试完成后应当删除记录(不用删除文件因为文件是同名文件无所谓)
+    await FileAvatar.destroy({
+      force: true,
+      where: {
+        uuid: {
+          [Op.in]: [firstAvatarUUID, body2.uuid],
+        },
+      },
+    });
   });
 });
