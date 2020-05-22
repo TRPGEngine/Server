d8f965f64ad2a1d6e44bfe73bf45f855374ee46d
moonrailgun <moonrailgun@gmail.com>
2020-05-22 11:57:24 +0800

    feat(player): 增加注册路由

--- packages/Player/lib/routers/user.ts
+++ packages/Player/lib/routers/user.ts
@@ -1,5 +1,6 @@
 import { TRPGRouter } from 'trpg/core';
 import { PlayerUser } from 'packages/Player/lib/models/user';
+import _ from 'lodash';
 const userRouter = new TRPGRouter();
 
 userRouter.get('/info/:uuid', async (ctx) => {
@@ -7,6 +8,17 @@ userRouter.get('/info/:uuid', async (ctx) => {
   const user = await PlayerUser.findByUUID(playerUUID);
 
   ctx.body = { user };
+});
+
+userRouter.post('/register', async (ctx) => {
+  const { username, password } = ctx.request.body;
+
+  if (_.isNil(username) || _.isNil(password)) {
+    throw new Error('缺少必要字段');
+  }
+
+  const results = await PlayerUser.registerUser(username, password);
+  ctx.body = { results };
 });
 
 export default userRouter;
