eaf98ca1cba8280bcab9712398300ec7dbd569af
moonrailgun <moonrailgun@gmail.com>
2020-05-23 13:44:56 +0800

    style(rtc): format

--- services/rtc/server/lib/interactiveClient.js
+++ services/rtc/server/lib/interactiveClient.js
@@ -3,17 +3,21 @@ const os = require('os');
 const path = require('path');
 
 const SOCKET_PATH_UNIX = '/tmp/mediasoup-demo.sock';
-const SOCKET_PATH_WIN = path.join('\\\\?\\pipe', process.cwd(), 'mediasoup-demo');
-const SOCKET_PATH = os.platform() === 'win32'? SOCKET_PATH_WIN : SOCKET_PATH_UNIX;
+const SOCKET_PATH_WIN = path.join(
+  '\\\\?\\pipe',
+  process.cwd(),
+  'mediasoup-demo'
+);
+const SOCKET_PATH =
+  os.platform() === 'win32' ? SOCKET_PATH_WIN : SOCKET_PATH_UNIX;
 
-module.exports = async function()
-{
-	const socket = net.connect(SOCKET_PATH);
+module.exports = async function() {
+  const socket = net.connect(SOCKET_PATH);
 
-	process.stdin.pipe(socket);
-	socket.pipe(process.stdout);
+  process.stdin.pipe(socket);
+  socket.pipe(process.stdout);
 
-	socket.on('connect', () => process.stdin.setRawMode(true));
-	socket.on('close', () => process.exit(0));
-	socket.on('exit', () => socket.end());
+  socket.on('connect', () => process.stdin.setRawMode(true));
+  socket.on('close', () => process.exit(0));
+  socket.on('exit', () => socket.end());
 };
