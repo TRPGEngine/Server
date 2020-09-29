9d1e5fd1a1b8ab3672438269800f7d6e8384165b
moonrailgun <moonrailgun@gmail.com>
2020-09-29 17:38:27 +0800

    refactor(group): 增加创建邀请码的路由

--- packages/Group/lib/routers/invite-code.ts
+++ packages/Group/lib/routers/invite-code.ts
@@ -9,7 +9,7 @@ const inviteCodeRouter = new TRPGRouter<{
 }>();
 
 /**
- * 获取团队会话记录
+ * 获取邀请码信息
  */
 inviteCodeRouter.get('/invite/code/:inviteCode/info', async (ctx) => {
   const inviteCode: string = ctx.params.inviteCode;
@@ -19,7 +19,22 @@ inviteCodeRouter.get('/invite/code/:inviteCode/info', async (ctx) => {
 });
 
 /**
- * 同意加入团
+ * 创建邀请码
+ */
+inviteCodeRouter.post('/invite/code/create', ssoAuth(), async (ctx) => {
+  const playerUUID = ctx.state.player.uuid;
+  const { groupUUID } = ctx.request.body;
+
+  if (_.isNil(groupUUID) || _.isNil(playerUUID)) {
+    throw new Error('缺少必要字段');
+  }
+
+  const invite = await GroupInviteCode.createInvite(groupUUID, playerUUID);
+  ctx.body = { invite };
+});
+
+/**
+ * 同意使用邀请码加入团
  */
 inviteCodeRouter.post(
   '/invite/code/:inviteCode/apply',
