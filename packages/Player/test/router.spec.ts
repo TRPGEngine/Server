f50281a4348f0e03754c16b7798bbefb575dcbc7
moonrailgun <moonrailgun@gmail.com>
2021-04-08 15:32:38 +0800

    test: fix testcase

--- packages/Player/test/router.spec.ts
+++ packages/Player/test/router.spec.ts
@@ -16,8 +16,8 @@ describe('Player router', () => {
     expect(body).toHaveProperty('user');
     expect(body.user).not.toHaveProperty('password');
     expect(body.user).not.toHaveProperty('salt');
-    expect(body.user).not.toHaveProperty('token');
-    expect(body.user).not.toHaveProperty('app_token');
+    expect(body.user).toHaveProperty('token', '');
+    expect(body.user).toHaveProperty('app_token', '');
     expect(body.user).not.toHaveProperty('last_ip');
   });
 });
